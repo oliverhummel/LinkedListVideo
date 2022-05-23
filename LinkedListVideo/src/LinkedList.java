26484d0f945e5a953d2e97f7ef722a2e3b143613
Oliver Hummel <oliver@192.168.127.203>
2022-05-23 16:48:40 +0200

    Einfügen in die Liste hinzugefügt.

--- LinkedListVideo/src/LinkedList.java
+++ LinkedListVideo/src/LinkedList.java
@@ -1,8 +1,24 @@
 
 public class LinkedList {
+	private Knoten erster;
+	
+	public LinkedList() {
+		erster = null;
+	}
 	
 	public void add(Student student) {
+		Knoten k = new Knoten(student);
 		
+		if (erster == null)
+			erster = k;
+		else {
+			Knoten aktueller = erster;
+			while (aktueller.nächster != null) {
+				aktueller = aktueller.nächster;
+			}
+			
+			aktueller.nächster = k;
+		}
 	}
 
 	public Student get(int index) {
@@ -14,6 +30,17 @@ public class LinkedList {
 	}
 
 	public void add(int i, Student s4) {
+		
+	}
+	
+	class Knoten {
+		private Student passagier;
+		private Knoten nächster;
+		
+		Knoten(Student s) {
+			nächster = null;
+			passagier = s;
+		}
 		
 	}
 
